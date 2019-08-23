5b4a31eba3da385cc521511c129c0ce9636df67d
mladjankg <mladjan.mihajlovickg@gmail.com>
2019-08-23 13:46:26 +0200

    added missing for context

---
+++ src/rs/ac/bg/etf/pp1/ForContext.java
@@ -0,0 +1,14 @@
+package rs.ac.bg.etf.pp1;
+
+import java.util.LinkedList;
+
+public class ForContext {
+	public LinkedList<Integer> conditionBackPatchAdresses = new LinkedList<Integer>();
+	public LinkedList<Integer> trueConditionBackPatchAdresses = new LinkedList<Integer>();
+	public LinkedList<Integer> falseConditionAdresses = new LinkedList<Integer>();
+	//public LinkedList<Integer> afterForDesigStatementAddresses = new LinkedList<Integer>();
+	//public int falseConditionAdress = 0;
+	public int conditionStatementAddress = 0;
+	public int afterForDesigStatementAddress= 0;
+	
+}
